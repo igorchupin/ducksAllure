00fcdf72e329f405c141aa71471daa1ecccd2fdd
Igor Chupin <i.chupin@21vek.by>
2023-04-13 20:59:06 +0300

    Allure

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,10 +3,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import pageOblects.MainPage;
 import pageOblects.RubberDucksPage;
 
@@ -14,20 +13,22 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.open;
 
+//@Listeners (Listener.class)
+
 public class BaseTest {
     protected String base_URL = "https://litecart.stqa.ru/en/";
     protected WebDriver webDriver;
     protected MainPage mainPage;
     protected RubberDucksPage rubberDucksPage;
     Logger logger = Logger.getLogger(BaseTest.class);
-
+/*
     @BeforeClass
     public void beforeClass() {
         mainPage = new MainPage(webDriver);
         rubberDucksPage = new RubberDucksPage(webDriver);
     }
-
-    @BeforeTest
+*/
+    @BeforeClass
     public void beforeTest() {
         logger.info("Before test started");
         WebDriverManager.chromedriver().setup();
@@ -35,18 +36,20 @@ public class BaseTest {
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(300));
         WebDriverRunner.setWebDriver(webDriver);
+        mainPage = new MainPage(webDriver);
+        rubberDucksPage = new RubberDucksPage(webDriver);
         logger.info("Before test ended");
     }
 
     @BeforeMethod
     public void beforeMethod() {
         logger.info("Before method deleting cookies");
-        webDriver.manage().deleteAllCookies();
+        WebDriverRunner.getWebDriver().manage().deleteAllCookies();
         logger.info("Openning " + base_URL);
         open(base_URL);
     }
 
-    @AfterTest
+    @AfterClass
     public void afterTest() {
         logger.info("Tests ended");
         webDriver.quit();
--- src/test/java/Listener.java
+++ src/test/java/Listener.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Listener implements ITestListener {
+
     Logger logger = Logger.getLogger(Listener.class);
     @Override
     public void onTestStart(ITestResult result) {
@@ -42,13 +43,17 @@ public class Listener implements ITestListener {
 
 
     public void getScreen() {
+        String fileName;
+        logger.info("Start getting screen");
         File screenshot = ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.FILE);
-        File res = new File("src/main/resources/screen_" + System.currentTimeMillis() + ".png");
+        fileName = "src/main/resources/screen_" + System.currentTimeMillis() + ".png";
+        File res = new File(fileName);
         try {
             FileUtils.copyFile(screenshot, res);
         } catch (
                 IOException e) {
             throw new RuntimeException(e);
         }
+        logger.info("Screen was saved to " + fileName);
     }
 }
